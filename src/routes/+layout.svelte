3f81b04901a2be016fa959466a2d79ef143c1ea2
TeejMcSteez <tjhall047@gmail.com>
2025-03-05 17:44:16 -0500

    Adding analytics from vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
     let { children } = $props();
     import "../app.css";
+    import { dev } from '$app/environment';
+    import { injectAnalytics  } from '@vercel/analytics/sveltekit';
+
+    injectAnalytics({ mode: dev ? 'development' : 'production'});
 </script>
 
 {@render children()}
