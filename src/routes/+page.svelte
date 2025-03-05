58f1681a33dd1b158e6d706be448deb2611cb588
TeejMcSteez <tjhall047@gmail.com>
2025-03-05 02:58:13 -0500

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,103 @@
+<div class="dark:bg-slate-900 overflow-auto">
+    <div class=" bg-gradient-to-r from-neutral-800 to-pink-600 text-white container mx-auto px-6 py-6 flex items-center justify-between max-w-screen">
+        <h1 class="text-4xl font-extrabold tracking-tight">Heathers Personal Website</h1>
+        <nav class="space-x-4">
+            <a href="#policies" class="hover:text-pink-300 transition-colors text-xl">Policies</a>
+            <a href="#location/contact" class="hover:text-pink-300 transition-colors text-xl">Contact</a>
+        </nav>
+    </div>
+    
+    <main class="container mx-auto px-6">
+        <section id="services" class="my-12 p-6 bg-white dark:bg-slate-800 dark:text-gray-100 rounded-lg shadow-md">
+            <h2 class="text-2xl font-bold mb-4">Services</h2>
+            <div id="extensions" class="mb-6">
+                <h3 class="text-xl font-semibold underline">Extensions</h3>
+                <p class="text-gray-600 dark:text-gray-300">Consultation Required (Prices provided at consultation)</p>
+                <ul class="list-disc ml-6 mt-2">
+                    <li class="text-gray-700 dark:text-gray-200 font-medium">$150+</li>
+                </ul>
+            </div>
+            <div id="haircuts" class="mb-6">
+                <h3 class="text-xl font-semibold underline">Haircuts</h3>
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div>
+                        <h4 class="font-semibold italic">Men</h4>
+                        <ul class="list-disc ml-6">
+                            <li>Hair Cut - $20</li>
+                            <li>Beard Trim - $10</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 class="font-semibold italic">Women</h4>
+                        <ul class="list-disc ml-6">
+                            <li>Dry Cut - $30</li>
+                            <li>Shampoo/Cut/Style - $35</li>
+                            <li>Shampoo Style - $35</li>
+                            <li>Conditioning Treatment - $35</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div id="color" class="mb-6">
+                <h3 class="text-xl font-semibold underline">Color & Foils</h3>
+                <p class="text-gray-600 dark:text-gray-300">Single color included (4oz) & additional usage at extra cost</p>
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-2">
+                    <div>
+                        <h4 class="font-medium">Color</h4>
+                        <ul class="list-disc ml-6">
+                            <li>Short Hair - $55</li>
+                            <li>Medium Hair - $65</li>
+                            <li>Long Hair - $85</li>
+                            <li>Conditioning Treatments - $35</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 class="font-medium">Foils</h4>
+                        <ul class="list-disc ml-6">
+                            <li>Partial: Short $65, Medium $75, Long $85</li>
+                            <li>Full: Short $75, Medium $85, Long $95</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <h4 class="font-medium">Toning</h4>
+                    <ul class="list-disc ml-6">
+                        <li>Short Hair - $25</li>
+                        <li>Medium Hair - $35</li>
+                        <li>Long Hair - $45</li>
+                    </ul>
+                </div>
+            </div>
+            <div id="waxing">
+                <h3 class="text-xl font-semibold underline">Waxing</h3>
+                <ul class="list-disc ml-6">
+                    <li>Eyebrows - $10</li>
+                    <li>Lips - $10</li>
+                    <li>Chin - $10</li>
+                    <li>Face - $30</li>                
+                </ul>
+            </div>
+        </section>
+        
+        <section id="policies" class="my-12 p-6 bg-white rounded-lg shadow-md dark:bg-slate-800 dark:text-gray-100">
+            <h2 class="text-2xl font-bold mb-4">Policies</h2>
+
+            <ul class="list-disc ml-6 mb-6 text-xl">
+                <li>Please no box dye soon before appoiment unless consulted</li>
+                <li>Please tell me if there is prior damage, issues, or any other problem so I can take note before hand and ensure that is properly addressed in your appoiment!</li>
+                <li>While I am flexible with clients in certain situations prices are final and non-negotiable</li>
+            </ul>
+
+            <p class="text-gray-700 dark:text-gray-200 text-2xl">Please sign the release form below to ensure that you are aware of my policies before booking an appointment for the first time.</p>
+            <a href="https://forms.gle/PH5UyD3apJJFgXVH9" class="text-3xl m-10 underline italic hover:animate-pulse">Please Click Here to Sign the Release Form for First Time Appoitments</a>
+        </section>
+        
+        <section id="location/contact" class="my-12 p-6 bg-white rounded-lg shadow-md dark:bg-slate-800 dark:text-gray-100">
+            <h2 class="text-2xl font-bold mb-4">Location & Contact</h2>
+            <p class="mb-4 text-gray-700 dark:text-gray-200">South City Street, Kings Mountain, North Carolina 28086</p>
+            <p class="mb-4">someone@gmail.com</p>
+            <p class="mb-4">Phone Number: 911-911-9111</p>
+            <footer class="mb-4">Please be respectful of my time as I am of yours, contact past 7pm cannot be guaranteed!</footer>
+        </section>
+    </main>
+</div>
