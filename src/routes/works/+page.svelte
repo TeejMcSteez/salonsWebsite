a9fa259744c9d261ed1a75a6ccb96620c1a964bd
TeejMcSteez <tjhall047@gmail.com>
2025-03-05 20:41:18 -0500

    Added route for work show off

---
+++ src/routes/works/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+    // import work1 from '$lib/assets/works/'
+</script>
+<div class="flex flex-row justify-between">
+    <h1 class="m-4">See my Works</h1>
+    <a href="/" class="m-4">Home</a>
+</div>
+
+<div class="container">
+
+    <div class="min-h-screen snap-y snap-mandatory">
+        <img src="#" alt="">
+    </div>
+
+    <div class="min-h-screen snap-y snap-mandatory">
+        <img src="#" alt="">
+    </div>
+
+    <div class="min-h-screen snap-y snap-mandatory">
+        <img src="#" alt="">
+    </div>
+
+    <div class="min-h-screen snap-y snap-mandatory">
+        <img src="#" alt="">
+    </div>
+
+    <div class="min-h-screen snap-y snap-mandatory">
+        <img src="#" alt="">
+    </div>
+
+    <div class="min-h-screen snap-y snap-mandatory">
+        <img src="#" alt="">
+    </div>
+
+    <div class="min-h-screen snap-y snap-mandatory">
+        <img src="#" alt="">
+    </div>
+
+    <div class="min-h-screen snap-y snap-mandatory">
+        <img src="#" alt="">
+    </div>
+    
+</div>
