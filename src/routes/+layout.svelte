1be16a3bdf92c964aade6f1b9f7a9fe177e43652
Alexis Clary <tjhall047@gmail.com>
2025-03-28 18:46:53 -0400

    Added vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,10 @@
     import { injectAnalytics  } from '@vercel/analytics/sveltekit';
 
     injectAnalytics({ mode: dev ? 'development' : 'production'});
+
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+    injectSpeedInsights();
 </script>
 
 {@render children()}
